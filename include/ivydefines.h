f4e81ac85ac890abcb89082d39eb0843047f0bbb
Allart Ian Vogelesang <ian.vogelesang@hitachivantara.com>
2018-08-15 10:44:21 -0700

    update version to 02.03.00 with adding HM850 support

--- include/ivydefines.h
+++ include/ivydefines.h
@@ -22,7 +22,7 @@
 #include <array>
 #include <string>
 
-const std::string ivy_version {"2.02.00"};
+const std::string ivy_version {"2.03.00"};
 
 //using ivy_int = long long int; using ivy_float = long double;
   using ivy_int =      long int; using ivy_float =      double;
